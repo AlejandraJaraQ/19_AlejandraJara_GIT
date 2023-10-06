dc763009d43b868b6d3c2957223f77877d4feb2b
AlejandraJaraQ <alejandra.jara@vallegrande.edu.pe>
2023-10-06 08:51:30 -0500

    Java_

---
+++ 19_AlejandraJara/src/S08_19/Main_S08_19.java
@@ -0,0 +1,10 @@
+
+package S08_19;
+
+public class Main_S08_19 {
+     public static void main(String[] args) {
+     // TODO code application logic here
+     
+     System.out.println("Hola mundo");
+    }
+}
---
+++ 19_AlejandraJara/src/S09_19/person6_19.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S09_19;
+
+/**
+ *
+ * @author aleja
+ */
+public class person6_19 {
+    
+}
